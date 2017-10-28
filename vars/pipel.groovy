054d16ad421e4b270dbca53402eff7a28ae49bda
secretter <secretter@sec.com>
2017-10-29 01:12:10 +0900

    no message

--- vars/pipel.groovy
+++ vars/pipel.groovy
@@ -1,6 +1,12 @@
-def call(Map hogee) {
+@Grab('org.yaml:snakeyaml:1.17')
+
+import org.yaml.snakeyaml.Yaml
+
+def call(def path) {
     echo 'shared library phase 2!!'
-    // echo "${hogee}"
-    echo "${hogee}".obj1
-    // echo "${hogee}".getClass().toString()
+
+    Yaml parser = new Yaml()
+    List example = parser.load(("${path}" as File).text)
+
+    example.each{println it.subject}
 }
