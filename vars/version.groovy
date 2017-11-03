dc0cb89c2b29f77b7e1e9ed56c66c368afa86f2a
secretter <secretter@sec.com>
2017-11-03 14:21:28 +0900

    no message

--- vars/version.groovy
+++ vars/version.groovy
@@ -1,5 +1,5 @@
 def call(ver) {
     echo "${ver}"
-    echo "git show-ref -s -d --abbrev ${ver} | awk '/\\^{}\\\$/ {print \\\$1}'"
+    echo "git show-ref -s -d --abbrev ${ver} | awk '/\\^{}\$/ {print \\\$1}'"
     echo "${vvv}"
 }
