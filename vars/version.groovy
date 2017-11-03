21d056b500c3fbbbd25478ca9a5a62340e427110
secretter <secretter@sec.com>
2017-11-03 15:08:33 +0900

    no message

--- vars/version.groovy
+++ vars/version.groovy
@@ -1,7 +1,10 @@
 def call(ver) {
     echo "${ver}"
     sh "git show-ref -s -d --abbrev ${ver} | awk '/\\^{\\}\$/ {print \$1}'"
-    def vvv = "git show-ref -s -d --abbrev ${ver} | awk '/\\^{\\}\$/ {print \$1}'".execute().text
+
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    "git show-ref -s -d --abbrev ${ver} | awk '/\\^{\\}\$/ {print \$1}'".execute().consumeProcessOutput(sout, serr)
     // def vvv = ['/bin/sh', '-c', "git show-ref -s -d --abbrev ${ver} | awk '/\\^{\\}\$/ {print \$1}'"].execute().text
-    echo "${vvv}"
+    echo "${sout}"
+    echo "${serr}"
 }
