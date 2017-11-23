3245f0bdce98093329209ab6d72055d1a91b379b
secretter <secretter@sec.com>
2017-11-23 10:23:48 +0900

    no message

--- vars/exx.groovy
+++ vars/exx.groovy
@@ -5,6 +5,10 @@ def call(Map conf) {
     echo 'called'
 
     Example ee = new Example(this, conf)
-    ee.exa()
+    if (ee.isDirExisting()) {
+        echo 'trueee'
+    } else {
+        echo 'falseee'
+    }
 
 }
