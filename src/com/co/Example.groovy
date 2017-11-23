3a1cd699e33c845e40fe39fb9c92635aab1ae406
secretter <secretter@sec.com>
2017-11-23 09:19:19 +0900

    no message

--- src/com/co/Example.groovy
+++ src/com/co/Example.groovy
@@ -1,10 +1,13 @@
 package com.co
 
-// import com.cloudbees.groovy.cps.NonCPS
+import com.cloudbees.groovy.cps.NonCPS
 
 class Example implements Serializable {
+
+    @NonCPS
     def exa(Map conf) {
         echo 'hogeeee'
         println conf
     }
+
 }
